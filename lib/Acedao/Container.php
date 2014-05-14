f911bc60c6cf8b6cf4727d7b86dab36fc047a27e
Joël Poulin <joel@voilab.org>
2014-05-14 13:26:59 +0200

    - rapatriement de l'encoding de la BD directement dans le DAO, dans la classe Database.

--- lib/Acedao/Container.php
+++ lib/Acedao/Container.php
@@ -9,12 +9,15 @@ class Container extends \Pimple {
 		parent::__construct();
 
 		$this['config'] = array_merge($config, array(
-			'mode' => 'production' // if "production": limit some errors, if "strict": raise more exceptions and is more verbose
+			'mode' => 'production', // if "production": limit some errors, if "strict": raise more exceptions and is more verbose
+            'encoding' => 'utf8'
 		));
 
 		$this['db'] = function($c) {
-			return new Database($c['config']['db']);
-		};
+			$db = new Database($c['config']['db']);
+            $db->execute("SET NAMES '" . $c['config']['encoding'] . "';");
+            return $db;
+        };
 
 		$this['query'] = $this->factory(function($c) {
 			return new Query($c);
